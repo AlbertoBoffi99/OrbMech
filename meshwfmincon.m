83a39213c27dd02f02bf73298213a4c8f60e3f17
AlbertoBoffi99 <alberto.boffi@skywarder.eu>
2021-12-26 12:00:29 +0100

    Added new main.m

--- meshwfmincon.m
+++ meshwfmincon.m
@@ -34,6 +34,10 @@ run config.m
 
 % declare flyby modelling function as handle
 JEV = @(dates) JEV_lamb_fb_lamb(dates, astro, options);
+% non linear function for GA
+optim.ga_nonlincon = @(x) rp_nonlinFcn(x, astro.RE, astro.h_atm);
+% non linear function for fmincon
+optim.fmincon_nonlincon = @(x) rp_nonlinFcn(x, astro.RE, astro.h_atm);
 
 %
 departure.window = linspace(departure.date_min, departure.date_max, optim.nmesh_dep);
@@ -55,7 +59,22 @@ for j = 1:1:size(departure.window,2);
         for z = 1:1:size(arrival.window,2);
 
             temp.dates = [departure.window(j), fly.window(k), arrival.window(z)];
-            temp.Dv = JEV(temp.dates);
+
+            % B constraint matrix for GA 
+            optim.ga_Bcon = [ - temp.dates(1) + departure.SW; ...
+            - temp.dates(2) + fly.SW; ...
+            - temp.dates(3) + arrival.SW; ...
+            temp.dates(1) + departure.SW; ...
+            temp.dates(2) + fly.SW; ...
+            temp.dates(3) + arrival.SW ];
+
+            % update fmincon guess according to ga result
+            fmincon_guess = temp.dates;
+    
+            % optimization of Dv using fmincon
+            [temp.dates, temp.Dv] = ...
+            fmincon(JEV, fmincon_guess, optim.ga_Acon, optim.ga_Bcon, ...
+            [], [], [], [], optim.fmincon_nonlincon, options.fmincon_options);
 
             if temp.Dv < results.Dv_min && ~out.nanflag
                 results.dates = temp.dates;
@@ -74,74 +93,13 @@ for j = 1:1:size(departure.window,2);
     end
 end
 
-delete temp.wb
+
+delete(temp.wb)
 clear j k z temp
 
 fprintf('\nOptimization with discretization mesh conlcuded\n')
 
 save('meshResults.mat', "results");
-
-%% GA and FMINCON OPTIMIZATION
-
-index = 1;
-out.rp_norm = results.rp_norm;
-% non linear function for GA
-optim.ga_nonlincon = @(x) rp_nonlinFcn(x, astro.RE, astro.h_atm);
-% non linear function for fmincon
-optim.fmincon_nonlincon = @(x) rp_nonlinFcn(x, astro.RE, astro.h_atm);
-
-temp.wb = waitbar(0, 'GA Optimization Running ...');
-
-for index = 1:1:optim.noptim
-
-    waitbar(index/optim.noptim, temp.wb);
-    
-    % B constraint matrix for GA 
-    optim.ga_Bcon = [ - results.dates(1) + departure.SW; ...
-        - results.dates(2) + fly.SW; ...
-        - results.dates(3) + arrival.SW; ...
-        results.dates(1) + departure.SW; ...
-        results.dates(2) + fly.SW; ...
-        results.dates(3) + arrival.SW ];
-    
-    % optimization of Dv using GA
-    [temp.dates, temp.Dv] = ...
-        ga(JEV, optim.ga_nvars, optim.ga_Acon, optim.ga_Bcon, ...
-        [], [], [], [], optim.ga_nonlincon, [], options.ga_options);
-
-    results
-    
-    % update fmincon guess according to ga result
-    fmincon_guess = temp.dates;
-    
-    % optimization of Dv using fmincon
-    [temp.dates, temp.Dv] = ...
-        fmincon(JEV, fmincon_guess, optim.ga_Acon, optim.ga_Bcon, ...
-        [], [], [], [], optim.fmincon_nonlincon, options.fmincon_options);
-
-    results
-
-    if temp.Dv < results.Dv_min
-        results.dates = temp.dates;
-        results.Dv_min = temp.Dv;
-        results.ViT1 = out.ViT1;
-        results.ViT2 = out.ViT2;
-        results.DvT1 = out.DvT1;
-        results.DvT2 = out.DvT2;
-        results.Dvfb = out.Dvfb;
-        results.rp = out.rp;
-        results.rp_norm = out.rp_norm;
-        results.vp_p = out.vp_p;
-        results.vp_m = out.vp_m;
-    end
-
-end
-
- fprintf('\nGenetic algorithm and gradient method optimization conlcuded\n')
-delete temp.wb
-clear index temp
-
-%-------------------------------------------------------------------------%
 
 
 % clock stopped
